d82e0a3cccc978b60d5e7c3670383aebe3b78147
Jens Gåsemyr Magnus <jegm@equinor.com>
2020-07-15 16:20:37 +0200

    store decorated classes give better error messages

--- xun/functions/store/store.py
+++ xun/functions/store/store.py
@@ -1,28 +1,42 @@
-from abc import ABC
-from abc import abstractmethod
 from collections.abc import MutableMapping
 import functools
 
 
-def store(cls):
-    class _store_wrapper(cls, MutableMapping):
+def store(orig_class):
+    cls = orig_class
+    if not issubclass(cls, MutableMapping):
+        cls = type(cls.__name__, (cls, MutableMapping), {})
+
+    class _store_wrapper:
         def __init__(self, *args, **kwargs):
-            super().__init__(self, *args, **kwargs)
-            self._deleted = set()
-            functools.update_wrapper(self, cls)
+            self._store_wrapped = cls(*args, **kwargs)
+            self._store_deleted = set()
+
+        def __getattr__(self, name):
+            return getattr(self._store_wrapped, name)
+
+        def __contains__(self, key):
+            return key in self._store_wrapped
+
         def __delitem__(self, key):
-            super().__delitem__(key)
-            self._deleted.add(key)
+            self._store_wrapped.__delitem__(key)
+            self._store_deleted.add(key)
+
         def __getitem__(self, key):
-            if key in self._deleted:
+            if key in self._store_deleted:
                 raise KeyError('{} has been deleted'.format(key))
-            return super().__getitem__(key)
+            print('key:', key, 'store:', self._store_wrapped)
+            return self._store_wrapped.__getitem__(key)
+
         def __setitem__(self, key, value):
-            if key in self._deleted:
+            if key in self._store_deleted:
                 raise KeyError('{} has been deleted'.format(key))
-            if key in self:
+            if key in self._store_wrapped:
                 raise KeyError('{} already set'.format(key))
-            return super().__setitem__(key, value)
+            return self._store_wrapped.__setitem__(key, value)
+
+    functools.update_wrapper(_store_wrapper, orig_class, updated=())
+    MutableMapping.register(_store_wrapper)
     return _store_wrapper
 
 

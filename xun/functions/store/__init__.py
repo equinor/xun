4598fb39c5a4fb81dbd2cc87a89d4c592e87a4ca
Jens Gåsemyr Magnus <jegm@equinor.com>
2020-11-10 18:05:03 +0100

    Filebased store named Disk

--- xun/functions/store/__init__.py
+++ xun/functions/store/__init__.py
@@ -2,6 +2,7 @@ from .store import Store
 from .store import NamespacedKey
 from .store_accessor import StoreAccessor
 
+from .disk import Disk
 from .diskcache import DiskCache
 from .memory import Memory
 from .redis import Redis

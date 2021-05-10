ddc29059cc904dd82a6c98b0fbf1f9e35b596223
Veronika Solteszova <ves@equinor.com>
2021-05-10 14:10:41 +0200

    Remove camille dependency from setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ if __name__ == '__main__':
         include_package_data=True,
 
         install_requires = [
-            'camille',
             'celery',
             'fastavro',
             'matplotlib',

ca348e1b833e1eddfdb6e8de6cd73f60afaea4b6
Jens Gåsemyr Magnus <jegm@equinor.com>
2021-08-05 14:27:23 +0200

    Astor has become a true dependency

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
         include_package_data=True,
 
         install_requires = [
+            'astor',
             'cookiecutter',
             'cryptography',
             'dask[distributed]',

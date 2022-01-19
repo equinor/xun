d59e38fe0494faff1533bbc538a20a7489615387
Jens Gåsemyr Magnus <jegm@equinor.com>
2022-01-19 12:27:21 +0100

    Higher order functions

--- xun/serialization/xun_types.py
+++ xun/serialization/xun_types.py
@@ -1,4 +1,5 @@
 from ..functions import CallNode
+from ..functions import SymbolicFunction
 from ..functions.store import NamespacedKey
 from .functor import IsoFunctor
 from .functor import _internal_type
@@ -43,6 +44,25 @@ class CallNodeFunctor(metaclass=IsoFunctor):
             'subscript': value.subscript,
             'args': value.args,
             'kwargs': value.kwargs,
+        }
+
+    def __invert__(cls):
+        return cls._Inverse
+
+
+@_internal_type(SymbolicFunction)
+class SymbolicFunctionFunctor(metaclass=IsoFunctor):
+    class _Inverse(metaclass=IsoFunctor):
+        def __call__(cls, value):
+            return SymbolicFunction(value['name'], value['hash'])
+
+        def __invert__(cls):
+            return SymbolicFunctionFunctor
+
+    def __call__(cls, value):
+        return {
+            'name': value.name,
+            'hash': value.hash,
         }
 
     def __invert__(cls):
